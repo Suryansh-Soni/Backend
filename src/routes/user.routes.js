6af22709dfbb7204c2819681b0a917d052808e98
Suryansh Soni <suryanshsoni92@gmail.com>
2025-07-27 14:54:48 +0530

    code corection s

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -35,10 +35,10 @@ router.route("/current-user").get(verifyJWT, getCurrentUser);
 router.route("/update-account").patch(verifyJWT, updateAccountDetails);
 router
   .route("/avatar")
-  .patch(verifyJWT, upload.single("/avatar"), updteUserAvatar);
+  .patch(verifyJWT, upload.single("avatar"), updteUserAvatar);
 router
   .route("/cover-image")
-  .patch(verifyJWT, upload.single("/coverimage"), updteUserCoverimage);
+  .patch(verifyJWT, upload.single("coverimage"), updteUserCoverimage);
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
 router.route("/history").get(verifyJWT, getWatchHistory);
 export default router;
